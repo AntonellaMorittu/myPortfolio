05c57dadd8232466c4d94d1ed8e932316a420a39
Antonella Morittu <morittuantonella@gmail.com>
2018-02-08 12:33:09 +0100

    image added to about_me section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,5 @@
-// This is where it all goes :)
+$(document).on('click','.scrollToComment',function(){
+       $('html, body').animate({
+           scrollTop: $("#scroll_about_me").offset().top
+       }, 2000);
+   });
