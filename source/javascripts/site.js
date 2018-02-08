aa81b57dce75d92fc6bd10d99e8574c6c31e5286
Antonella Morittu <morittuantonella@gmail.com>
2018-02-08 22:09:30 +0100

    restyle and refactoring

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,17 +4,13 @@ $(document).ready(function() {
             scrollTop: $("#about_me_section").offset().top
         }, 1000);
     });
-});
 
-$(document).ready(function() {
     $("#projects_nav").click(function() {
         $('html, body').animate({
             scrollTop: $("#projects_section").offset().top
         }, 1000);
     });
-});
 
-$(document).ready(function() {
     $("#contacts_nav").click(function() {
         $('html, body').animate({
             scrollTop: $("#contacts_section").offset().top
