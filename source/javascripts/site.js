1e0d48367490900b213b15b0d0ae6cbc2f79e076
Antonella Morittu <morittuantonella@gmail.com>
2018-02-10 13:32:45 +0100

    cv section implemented sticky nav bar added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
     $("#about_me_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#about_me_section").offset().top
+            scrollTop: $("#about_me_section").offset().top - 50
         }, 1000);
     });
 
     $("#projects_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#projects_section").offset().top
+            scrollTop: $("#projects_section").offset().top - 80
         }, 1000);
     });
 
     $("#skills_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#skills_section").offset().top
+            scrollTop: $("#skills_section").offset().top - 40
         }, 1000);
     });
 
     $("#cv_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#cv_section").offset().top
+            scrollTop: $("#cv_section").offset().top + 20
         }, 1000);
     });
 
