6fb511121da09f01de08e8ba2c8d462a9fdbedaf
Antonella <antonella@technocreatives.com>
2019-03-04 14:37:28 +0100

    more resizing

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
     $("#about_me_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#about_me_section").offset().top - 50
+            scrollTop: $("#about_me_section").offset().top - 150
         }, 1000);
     });
 
     $("#projects_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#projects_section").offset().top - 150
+            scrollTop: $("#projects_section").offset().top - 130
         }, 1000);
     });
 
     $("#skills_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#skills_section").offset().top - 150
+            scrollTop: $("#skills_section").offset().top - 110
         }, 1000);
     });
 
     $("#cv_nav").click(function() {
         $('html, body').animate({
-            scrollTop: $("#cv_section").offset().top - 15
+            scrollTop: $("#cv_section").offset().top - 90
         }, 1000);
     });
 
