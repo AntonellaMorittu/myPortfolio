e544c45edb5dde6aa4b524f89b7606106956b974
AntonellaMorittu <antonella@technocreatives.com>
2020-12-21 10:51:37 +0100

    Automated commit at 2020-12-21 09:51:37 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#about_me_nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about_me_section").offset().top - 150
+        }, 1000);
+    });
+
+    $("#projects_nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects_section").offset().top - 150
+        }, 1000);
+    });
+
+    $("#skills_nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skills_section").offset().top - 110
+        }, 1000);
+    });
+
+    $("#cv_nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#cv_section").offset().top - 90
+        }, 1000);
+    });
+
+    $("#contacts_nav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contacts_section").offset().top
+        }, 1000);
+    });
+});
